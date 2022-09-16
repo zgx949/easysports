8a44a16d3378e68ede00ec319fcc5c7544478aa6
zgx <1637349696@qq.com>
2022-09-16 18:36:25 +0800

    fix: 用户id判断问题

--- sport-game/src/main/java/com/ruoyi/system/controller/SportRegistrationsController.java
+++ sport-game/src/main/java/com/ruoyi/system/controller/SportRegistrationsController.java
@@ -1,48 +1,37 @@
 package com.ruoyi.system.controller;
 
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
-import javax.servlet.http.HttpServletResponse;
-
-import com.ruoyi.common.exception.ServiceException;
-import com.ruoyi.common.core.domain.entity.SysUser;
-import com.ruoyi.common.core.domain.model.LoginUser;
-import com.ruoyi.common.core.redis.RedisCache;
-import com.ruoyi.common.utils.DateUtils;
-import com.ruoyi.common.utils.SecurityUtils;
-import com.ruoyi.system.domain.SportFields;
-import com.ruoyi.system.domain.SportGames;
-import com.ruoyi.system.domain.SportItem;
-import com.ruoyi.system.domain.Vo.UserSportGradeVo;
-import com.ruoyi.system.service.*;
-import org.apache.commons.lang3.ObjectUtils;
-import org.springframework.cache.annotation.Cacheable;
-import com.ruoyi.system.domain.Vo.GameSequenceBookVO;
-import com.ruoyi.system.domain.dto.UpdateGamesScoreDto;
-import com.ruoyi.system.service.ISportFieldsService;
-import com.ruoyi.system.service.ISportGamesService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import com.ruoyi.common.annotation.Log;
 import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
-import com.ruoyi.common.enums.BusinessType;
-import com.ruoyi.system.domain.SportRegistrations;
-import com.ruoyi.system.service.ISportRegistrationsService;
-import com.ruoyi.common.utils.poi.ExcelUtil;
+import com.ruoyi.common.core.domain.entity.SysUser;
 import com.ruoyi.common.core.page.TableDataInfo;
+import com.ruoyi.common.core.redis.RedisCache;
+import com.ruoyi.common.enums.BusinessType;
+import com.ruoyi.common.exception.ServiceException;
+import com.ruoyi.common.utils.DateUtils;
+import com.ruoyi.common.utils.SecurityUtils;
+import com.ruoyi.common.utils.poi.ExcelUtil;
+import com.ruoyi.system.domain.SportGames;
+import com.ruoyi.system.domain.SportItem;
+import com.ruoyi.system.domain.SportRegistrations;
+import com.ruoyi.system.domain.Vo.GameSequenceBookVO;
+import com.ruoyi.system.domain.Vo.UserSportGradeVo;
+import com.ruoyi.system.domain.dto.UpdateGamesScoreDto;
+import com.ruoyi.system.service.ISportGamesService;
+import com.ruoyi.system.service.ISportItemService;
+import com.ruoyi.system.service.ISportRegistrationsService;
+import com.ruoyi.system.service.ISysUserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang3.ObjectUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 报名管理Controller
@@ -164,7 +153,6 @@ public class SportRegistrationsController extends BaseController
         sportRegistrations.setUserId(SecurityUtils.getUserId());
         List<SportRegistrations> sportRegistrationsList= sportRegistrationsService.userRegisterationslist(sportRegistrations);
         startPage();
-        List<SportRegistrations> sportRegistrationsList= sportRegistrationsService.userRegisterationslist(sportRegistrations);
         return getDataTable(sportRegistrationsList);
     }
 
@@ -188,7 +176,7 @@ public class SportRegistrationsController extends BaseController
         SportRegistrations userSportRegistrations=new SportRegistrations();//用于从集合中获得用户的报名信息
         //遍历集合从中获取当前用户成绩
         for(SportRegistrations temp:sportRegistrationsList){
-            if(SecurityUtils.getUserId()==temp.getUserId()){
+            if(SecurityUtils.getUserId().equals(temp.getUserId())){
                 userSportRegistrations=temp;
             }
         }
