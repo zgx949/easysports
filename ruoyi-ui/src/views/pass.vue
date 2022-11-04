203ac428c7348d8b35f20c1c24db89c6e7414a1a
zgx <1637349696@qq.com>
2022-11-04 18:17:13 +0800

    feat: 完成查询功能

--- ruoyi-ui/src/views/pass.vue
+++ ruoyi-ui/src/views/pass.vue
@@ -33,7 +33,7 @@
             <i class="el-icon-question"></i>
             性别
           </template>
-          <el-tag size="small">{{ userInfo.sex == 0 ? '男' : userInfo.sex == 1 ? '女' : '未知'}}</el-tag>
+          <el-tag size="small">{{ userInfo.sex == 0 ? '男' : userInfo.sex == 1 ? '女' : '未知' }}</el-tag>
         </el-descriptions-item>
         <el-descriptions-item>
           <template slot="label">
@@ -51,19 +51,54 @@
         </el-descriptions-item>
       </el-descriptions>
     </div>
+    <el-dialog
+      title="请先设置学号绑定设备"
+      :visible.sync="dialogVisible"
+      width="90%">
 
+      <el-form :rules="rules" ref="ruleForm">
+        <el-form-item label="请输入学号" prop="userId">
+          <el-input v-model="userId"></el-input>
+        </el-form-item>
+      </el-form>
+<!--      <el-input v-model="userId">请输入学号</el-input>-->
+      <span slot="footer" class="dialog-footer">
+<!--        <el-button @click="dialogVisible = false">取 消</el-button>-->
+        <el-button type="primary" @click="setUserId">确 定</el-button>
+      </span>
+    </el-dialog>
     <div class="records">
       <el-divider>成绩记录</el-divider>
       <div v-for="(item, index) in scores" :key="index">
         <el-card class="box-card">
           <el-descriptions :title="item.activityName" :column="1" border>
-            <el-descriptions-item label="身高CM" label-class-name="my-label" content-class-name="my-content">{{ item.height }}</el-descriptions-item>
+            <el-descriptions-item label="身高CM" label-class-name="my-label" content-class-name="my-content">
+              {{ item.height }}
+            </el-descriptions-item>
             <el-descriptions-item label="体重KG">{{ item.weight }}</el-descriptions-item>
-            <el-descriptions-item label="肺活量"><el-tag :type="item.vitalCapacityPass? 'success' : 'danger'">{{ item.vitalCapacityPass? '合格' : '不合格' }}</el-tag></el-descriptions-item>
-            <el-descriptions-item label="立定跳"><el-tag :type="item.longJumpPass? 'success' : 'danger'">{{ item.longJumpPass? '合格' : '不合格' }}</el-tag></el-descriptions-item>
-            <el-descriptions-item label="坐位体前屈"><el-tag :type="item.sittingBodyBendPass? 'success' : 'danger'">{{ item.sittingBodyBendPass? '合格' : '不合格' }}</el-tag></el-descriptions-item>
-            <el-descriptions-item label="50米"><el-tag :type="item.fiftyRunPass? 'success' : 'danger'">{{ item.fiftyRunPass? '合格' : '不合格' }}</el-tag></el-descriptions-item>
-            <el-descriptions-item label="耐力跑"><el-tag :type="item.enduranceRunningPass? 'success' : 'danger'">{{ item.enduranceRunningPass? '合格' : '不合格' }}</el-tag></el-descriptions-item>
+            <el-descriptions-item label="肺活量">
+              <el-tag :type="item.vitalCapacityPass? 'success' : 'danger'">{{
+                  item.vitalCapacityPass ? '合格' : '不合格'
+                }}
+              </el-tag>
+            </el-descriptions-item>
+            <el-descriptions-item label="立定跳">
+              <el-tag :type="item.longJumpPass? 'success' : 'danger'">{{ item.longJumpPass ? '合格' : '不合格' }}</el-tag>
+            </el-descriptions-item>
+            <el-descriptions-item label="坐位体前屈">
+              <el-tag :type="item.sittingBodyBendPass? 'success' : 'danger'">{{
+                  item.sittingBodyBendPass ? '合格' : '不合格'
+                }}
+              </el-tag>
+            </el-descriptions-item>
+            <el-descriptions-item label="50米">
+              <el-tag :type="item.fiftyRunPass? 'success' : 'danger'">{{ item.fiftyRunPass ? '合格' : '不合格' }}</el-tag>
+            </el-descriptions-item>
+            <el-descriptions-item label="耐力跑">
+              <el-tag :type="item.enduranceRunningPass? 'success' : 'danger'">
+                {{ item.enduranceRunningPass ? '合格' : '不合格' }}
+              </el-tag>
+            </el-descriptions-item>
           </el-descriptions>
         </el-card>
       </div>
@@ -73,7 +108,9 @@
 
 <script>
 import logoImg from '@/assets/logo/logo.png'
-import { queryPass } from "@/api/system/grade";
+import {queryPass} from "@/api/system/grade";
+import {getData} from "@/api/system/dict/data";
+import userInfo from "@/views/system/user/profile/userInfo";
 
 export default {
   name: "pass.vue",
@@ -82,19 +119,58 @@ export default {
       logo: logoImg,
       userInfo: {},
       scores: [],
+      userId: '',
+      dialogVisible: false,
+      rules: {
+        userId: [
+          { required: true, message: '请输入7位数学号', trigger: 'blur' },
+          { min: 7, max: 7, message: '请输入7位数学号', trigger: 'blur' }
+        ],
+      }
     }
   },
   mounted() {
-    queryPass("0220465").then(res => {
-      if (res.code === 200) {
-        this.$message.success(res.msg);
-        this.userInfo = res.data.userInfo;
-        this.scores = res.data.scores
+    const userId = localStorage.getItem('userId');
+    if (!userId) {
+      this.dialogVisible = true;
+      this.$message.info("请先绑定学号");
+    } else {
+      this.getData(userId);
+    }
+  },
+  methods: {
+    setUserId () {
+      if (/[054]\d{6}/.test(this.userId)) {
+        this.getData(this.userId);
+        this.dialogVisible = false;
       } else {
-        this.$message.error(res.msg);
+        this.$message.error("学号位数或格式错误")
       }
-      console.log(res.data);
-    })
+    },
+    getData(userId) {
+      localStorage.setItem('userId', userId);
+      queryPass(userId).then(res => {
+        if (res.code === 200) {
+          this.$message.success(res.msg);
+          this.userInfo = res.data.userInfo;
+          this.scores = res.data.scores;
+          if (this.scores.length === 0) {
+            localStorage.removeItem('userId');
+          }
+        } else {
+          this.$message.error(res.msg);
+          localStorage.removeItem('userId');
+        }
+        console.log(res.data);
+      })
+    },
+    handleClose(done) {
+      this.$confirm('确认关闭？')
+        .then(_ => {
+          done();
+        })
+        .catch(_ => {});
+    }
   }
 }
 </script>
@@ -103,6 +179,7 @@ export default {
 .records {
   padding: 1rem;
 }
+
 .main {
   /*background-image: url("https://img.dowebok.com/9560.jpg");*/
   width: 100%;
@@ -113,6 +190,7 @@ export default {
 .info {
   padding: 1rem;
 }
+
 el-row {
   margin: 1rem;
 }
