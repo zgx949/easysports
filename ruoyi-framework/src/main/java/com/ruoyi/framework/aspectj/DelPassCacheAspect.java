7174184b425404187ece2319c019a3e6a7280961
zgx <1637349696@qq.com>
2022-11-04 13:55:00 +0800

    feat: 自动删除成绩通过情况缓存的切面

--- ruoyi-framework/src/main/java/com/ruoyi/framework/aspectj/DelPassCacheAspect.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/aspectj/DelPassCacheAspect.java
@@ -1,17 +1,26 @@
 package com.ruoyi.framework.aspectj;
 
+import com.alibaba.fastjson2.JSONArray;
+import com.alibaba.fastjson2.JSONObject;
 import com.ruoyi.common.annotation.DelPassCache;
 import com.ruoyi.common.annotation.Log;
+import com.ruoyi.common.core.redis.RedisCache;
 import com.ruoyi.common.utils.Threads;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @program: Ruoyi-Vue
@@ -24,6 +33,9 @@ import java.util.List;
 public class DelPassCacheAspect {
     private static final Logger logger = LoggerFactory.getLogger(Threads.class);
 
+    @Autowired
+    private RedisCache redisCache;
+
     /**
      * 处理完请求后执行
      *
@@ -32,7 +44,15 @@ public class DelPassCacheAspect {
     @AfterReturning(pointcut = "@annotation(controllerDelPassCache)", returning = "jsonResult")
     public void doAfterReturning(JoinPoint joinPoint, DelPassCache controllerDelPassCache, Object jsonResult)
     {
-        Object[] args = joinPoint.getArgs();
-        logger.info("查询体测通过情况参数：" + args.toString());
+        String jsonString = JSONObject.toJSONString(joinPoint.getArgs()[0]);
+        List list = JSONArray.parseArray(jsonString);
+        for (Object o : list) {
+            Map<String, Object> mp = (Map<String, Object>) o;
+            String userId = String.valueOf(mp.get("userId"));
+
+            // 删除缓存
+            boolean delResult = redisCache.deleteObject("fitness:pass" + userId);
+            logger.info("学号：{}, 存在新成绩录入, 删除缓存:{}", userId, delResult);
+        }
     }
 }
